41867aa143286d5435d01381adbce940f759f6de
higashidesign20260608 <higashidesign20260608@gmail.com>
2026-07-23 15:43:47 +0900

    syuusei

--- src/utils/content.ts
+++ src/utils/content.ts
@@ -6,19 +6,19 @@ import type { TypeNews } from "../types/type-content-collection";
 export function getLatestBlogs(num: number): TypeBlog[] {
   let count = 0;
   if ( num > BLOG_COLLECTION.length) {
-    count = (BLOG_COLLECTION.length - 1)
+    count = BLOG_COLLECTION.length;
   }else{
-    count = (num - 1)
+    count = num;
   };
-  return [...BLOG_COLLECTION].sort((a, b) => new Date(b.data.publishedAt).getTime() - new Date(a.data.publishedAt).getTime()).slice(0, num);
+  return [...BLOG_COLLECTION].sort((a, b) => new Date(b.data.publishedAt).getTime() - new Date(a.data.publishedAt).getTime()).slice(0, count);
 };
 
 export function getLatestNews(num: number): TypeNews[] {
   let count = 0;
   if ( num > NEWS_COLLECTION.length) {
-    count = (NEWS_COLLECTION.length - 1)
+    count = NEWS_COLLECTION.length;
   }else{
-    count = (num - 1)
+    count = num;
   };
-  return [...NEWS_COLLECTION].reverse().slice(0, num);
+  return [...NEWS_COLLECTION].sort((a, b) => new Date(b.data.publishedAt).getTime() - new Date(a.data.publishedAt).getTime()).slice(0, count);
 };
